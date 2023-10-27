8008f7636df0998877cde595cfef7db7d379ceea
kaiii299 <Zhikai299@gmail.com>
2023-10-28 01:03:17 +0800

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
+// import Navbar from '@/components/Navbar'
+import { cn } from '@/lib/utils'
+import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Breadit',
