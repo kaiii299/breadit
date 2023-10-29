6d5478c4ff44df2f735425c357a362c1108de9f4
kaiii299 <Zhikai299@gmail.com>
2023-10-29 18:23:11 +0800

    Added Route Guard

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link'
 const SignIn = () => {
   return (
     <div className='container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
+      <div>
+      </div>
       <div className='flex flex-col space-y-2 text-center'>
       <Icons.logo  className='mx-auto lg:w-1/2 md:w-1/3 w-1/2'/>
         <h1 className='text-2xl font-semibold tracking-tight'>Welcome back</h1>
@@ -13,14 +15,6 @@ const SignIn = () => {
         </p>
       </div>
       <UserAuthForm />
-      <p className='px-8 text-center text-sm text-muted-foreground'>
-        New to Breaddit?{' '}
-        <Link
-          href='/sign-up'
-          className='hover:text-brand text-sm underline underline-offset-4'>
-          Sign Up
-        </Link>
-      </p>
     </div>
   )
 }
